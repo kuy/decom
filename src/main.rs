22db2c198718568a281794232151391b0acdd368
Yuki Kodama <endflow.net@gmail.com>
2021-07-22 16:43:41 +0900

    Add tui

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 use futures::{stream, StreamExt};
 use std::{error::Error, result::Result};
+use tui::backend::CrosstermBackend;
+use tui::text::Spans;
+use tui::widgets::{Block, Borders, Paragraph, Wrap};
+use tui::Terminal;
 
 mod docker;
 mod docker_compose;
@@ -9,6 +13,21 @@ use log_collector::LogCollector;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
+    std::thread::spawn(|| {
+        let stdout = std::io::stdout();
+        let backend = CrosstermBackend::new(stdout);
+        if let Ok(mut terminal) = Terminal::new(backend) {
+            terminal.clear();
+            terminal.draw(|f| {
+                let size = f.size();
+                let block = Block::default().title("Block").borders(Borders::ALL);
+                let text = vec![Spans::from("This is a paragraph with several lines. You can change style your text the way you want.")];
+                let paragraph = Paragraph::new(text).block(block).wrap(Wrap { trim: true});
+                f.render_widget(paragraph, size);
+            });
+        }
+    });
+
     let services = docker_compose::services().await?;
     println!("main: services: {:?}", services);
 
@@ -19,11 +38,11 @@ async fn main() -> Result<(), Box<dyn Error>> {
         collectors.push(collector);
         println!("main: collector: '{}' started", s.service_name);
     });
-
-    let mut collectors = stream::select_all(collectors);
-    while let Some((service_name, total, diff)) = collectors.next().await {
-        println!("main: {}: {} [+{}]", service_name, total, diff);
-    }
-
+    /*
+        let mut collectors = stream::select_all(collectors);
+        while let Some((service_name, total, diff)) = collectors.next().await {
+            println!("main: {}: {} [+{}]", service_name, total, diff);
+        }
+    */
     Ok(())
 }
